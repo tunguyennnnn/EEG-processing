cd20ab5c9e9c6652504b80edbd00368200ebc824
tunguyennnnn <conghoa522@gmail.com>
2017-03-06 18:24:05 -0500

    add drone and main communication

--- BackEnd/drone_input.js
+++ BackEnd/drone_input.js
@@ -1,25 +1,29 @@
-// process.stdin.resume();
-// process.stdin.setEncoding('utf8');
-// var util = require('util');
-//
-// process.stdin.on('data', function (text) {
-//   console.log('received data:' + text);
-//   if (text === 'quit\n') {
-//     done();
-//   }
-// });
-//
-// function done() {
-//
-//   process.exit();
-// }
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+const util = require('util');
 
-const net = require('net');
+process.stdin.on('data', function (command) {
+  let command = command.replace('\n', '');
+  // do something with the command here, command is string : "1", "2", "3" etc
 
 
-net.createServer(function(socket){
-  let command = ''
-  socket.on('data', function(data){
-    command = 'XXX' + data;
-  });
-}).listen(5000);
+  // quit is to stop connecting to the drone. This should land the drone safely.
+  if (command === 'quit') {
+    done();
+  }
+});
+
+function done() {
+
+  process.exit();
+}
+
+// const net = require('net');
+//
+//
+// net.createServer(function(socket){
+//   let command = ''
+//   socket.on('data', function(data){
+//     command = 'XXX' + data;
+//   });
+// }).listen(5000);
